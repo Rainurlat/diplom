cf9d5b876e265128ffc439dd27ebb4ae990902fc
Sadykova Rozaliya <rooozaliya@mail.ru>
2022-01-12 23:01:46 +0300

    add DBforTwo

--- src/main/java/com/example/diplom/models/Login.java
+++ src/main/java/com/example/diplom/models/Login.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 
 public class Login {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String login, password;
