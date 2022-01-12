a8281fdfb348d8cba73faa30f07974f5972f22e0
Sadykova Rozaliya <rooozaliya@mail.ru>
2022-01-12 22:04:32 +0300

    add DB

---
+++ src/main/java/com/example/diplom/models/Login.java
@@ -0,0 +1,40 @@
+package com.example.diplom.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String login, password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
